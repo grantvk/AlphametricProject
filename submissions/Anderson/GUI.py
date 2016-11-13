fa7fd106dd5cc32ed1dc74a790615c9792872387
joelamartina <joseph.lamartina>
2016-11-13 14:58:38 -0600

    Renamed CSP???? as GoogleCSPSolver and imported the solver to the GUI file in Anderson

--- submissions/Anderson/GUI.py
+++ submissions/Anderson/GUI.py
@@ -3,6 +3,7 @@
 # See the License for the specific language governing permissions and limitations under the License.
 from tkinter import *
 from tkinter import ttk
+from submissions.Anderson import GoogleCSPSolver
 
 
 # def calculate(*args):
